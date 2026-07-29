e68affec00cd880fab9eeac3e1e0c00353458197
Somiya Chaudhary <somiyachaudhary41@gmail.com>
2026-07-29 23:04:34 +0530

    find max

--- Day1.java
+++ Day1.java
@@ -1,6 +1,20 @@
-public static void main(String[] args) {
-    int[] arr={1,4,6,7};
-    for(int i=0; i<arr.length; i++){
-        System.out.println(arr[i]);
+//print all elements in an array
+// public static void main(String[] args) {
+//     int[] arr={1,4,6,7};
+//     for(int i=0; i<arr.length; i++){
+//         System.out.println(arr[i]);
+//     }
+// }
+
+//find max in an array
+public static void main(String[] args){
+    int[] arr={1,5,7,25,9};
+    int max=arr[0];
+
+    for(int i=1; i<arr.length; i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
     }
+    System.out.println("Max element in the array is: "+max);
 }
