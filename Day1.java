2ab8ebc743dde6f5dbbc34af3a270fe09e8103ed
Somiya Chaudhary <somiyachaudhary41@gmail.com>
2026-07-29 23:21:05 +0530

    sum of all elements

--- Day1.java
+++ Day1.java
@@ -6,11 +6,11 @@
 //     }
 // }
 
+
 //find max in an array
 // public static void main(String[] args){
 //     int[] arr={1,5,7,25,9};
 //     int max=arr[0];
-
 //     for(int i=1; i<arr.length; i++){
 //         if(arr[i]>max){
 //             max=arr[i];
@@ -19,15 +19,27 @@
 //     System.out.println("Max element in the array is: "+max);
 // }
 
-//find min element in an array
 
+//find min element in an array
+// public static void main(String[] args){
+//     int[] arr={1,3,7,-4,-10};
+//     int min=arr[0];
+//     for(int i=0; i<arr.length; i++){
+//         if(arr[i]<min){
+//             min=arr[i];
+//         }
+//     }
+//     System.out.println(min);
+// }
+
+
+//find sum of all elements in an array
 public static void main(String[] args){
-    int[] arr={1,3,7,-4,-10};
-    int min=arr[0];
+    int[] arr={1,2,3,4,5};
+    int sum=0;
+
     for(int i=0; i<arr.length; i++){
-        if(arr[i]<min){
-            min=arr[i];
-        }
+        sum=sum+arr[i];
     }
-    System.out.println(min);
+    System.out.println(sum);
 }
