4d14e302e0ba921932534250063d07aa258fcac5
Somiya Chaudhary <somiyachaudhary41@gmail.com>
2026-07-29 23:50:04 +0530

    odd and even count

--- Day1.java
+++ Day1.java
@@ -46,12 +46,33 @@
 
 
 //find average of all elements
-public static void main(String[] args){
-    int[] arr={1,2,3,4,5};
-    int sum=0;
+// public static void main(String[] args){
+//     int[] arr={1,2,3,4,5};
+//     int sum=0;
 
-    for(int i=0; i<arr.length; i++){
-        sum=sum+arr[i];
+//     for(int i=0; i<arr.length; i++){
+//         sum=sum+arr[i];
+//     }
+//     System.out.println(sum/arr.length);
+// }
+
+
+//count odd and even in an array
+public static void main(String[] args){
+
+    int[] arr={1,2,3,5,7,5,6,3,4,8,9,123};
+
+    int even=0;
+    int odd=0;
+
+    for(int i=0;i<arr.length; i++){
+        if(arr[i]%2==0){
+            even++;
+            System.out.print(arr[i]);
+        }else{
+            odd++;
+        }
     }
-    System.out.println(sum/arr.length);
+    System.out.println(even);
+    System.out.print(odd);
 }
