3b6630786f654b2c01b24d0cca4e0754653f57c7
Somiya Chaudhary <somiyachaudhary41@gmail.com>
2026-07-30 23:54:56 +0530

    day2

--- Day1.java
+++ Day1.java
@@ -200,23 +200,26 @@
 
 //count positive negative and zero in an array
 
-public class Main{
-    public static void main(String[] args){
-        int[] arr={-1,-8,-9,-9,0,9,6,4,0,-8};
-        int pos=0;
-        int neg=0;
-        int zero=0;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]>0){
-                pos++;
-            }else if(arr[i]<0){
-                neg++;
-            }else{
-                zero++;
-            }
-        }
-        System.out.println("Positive: "+pos);
-        System.out.println("Negative: "+neg);
-        System.out.println("Zero: "+zero);
-    }
-}
+// public class Main{
+//     public static void main(String[] args){
+//         int[] arr={-1,-8,-9,-9,0,9,6,4,0,-8};
+//         int pos=0;
+//         int neg=0;
+//         int zero=0;
+//         for(int i=0; i<arr.length; i++){
+//             if(arr[i]>0){
+//                 pos++;
+//             }else if(arr[i]<0){
+//                 neg++;
+//             }else{
+//                 zero++;
+//             }
+//         }
+//         System.out.println("Positive: "+pos);
+//         System.out.println("Negative: "+neg);
+//         System.out.println("Zero: "+zero);
+//     }
+// }
+
+
+//find the frequency of an element in an array
