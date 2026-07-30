cb2c8aadd42e837ff7d54175ac43945eeceb58f7
Somiya Chaudhary <somiyachaudhary41@gmail.com>
2026-07-31 00:06:11 +0530

    remove duplicate

--- Day2.java
+++ Day2.java
@@ -1,3 +1,19 @@
+// remove duplicates from a sorted array
 public class Day2 {
-    
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5,6,7,8, 8,9,9};
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]==arr[j]){
+                    arr[j]=0;
+                }
+            }
+        }
+        System.out.println("After removing duplicates");
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]!=0){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
 }
