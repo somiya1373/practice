411998bd36b14008062e59a126b34d96f44a796e
Somiya Chaudhary <somiyachaudhary41@gmail.com>
2026-08-05 15:08:30 +0530

    merge two sorted arrays into one

--- Day3.java
+++ Day3.java
@@ -26,100 +26,153 @@
 
 // Create a Singly Linked List and perform basic operations
 
-class Node {
-    int data;
-    Node next;
+// class Node {
+//     int data;
+//     Node next;
 
-    Node(int data) {
-        this.data = data;
-        this.next = null;
-    }
-}
+//     Node(int data) {
+//         this.data = data;
+//         this.next = null;
+//     }
+// }
+
+// public class Day3 {
+
+//     public static void main(String[] args) {
+
+//         // Creating Linked List: 1 -> 2 -> 3 -> 4
+//         Node head = new Node(1);
+//         head.next = new Node(2);
+//         head.next.next = new Node(3);
+//         head.next.next.next = new Node(4);
+
+//         System.out.println("Original Linked List:");
+//         traverse(head);
+
+//         // Insertion
+//         head = insertAtEnd(head, 5);
+//         System.out.println("\nAfter Insertion at End:");
+//         traverse(head);
+
+//         // Deletion
+//         head = deleteNode(head, 3);
+//         System.out.println("\nAfter Deleting 3:");
+//         traverse(head);
+//     }
+
+//     // Traversal
+//     public static void traverse(Node head) {
+//         Node current = head;
+
+//         while (current != null) {
+//             System.out.print(current.data + " -> ");
+//             current = current.next;
+//         }
+
+//         System.out.println("null");
+//     }
+
+//     // Insert at End
+//     public static Node insertAtEnd(Node head, int data) {
+
+//         Node newNode = new Node(data);
+
+//         if (head == null) {
+//             return newNode;
+//         }
+
+//         Node current = head;
+
+//         while (current.next != null) {
+//             current = current.next;
+//         }
+
+//         current.next = newNode;
+
+//         return head;
+//     }
+
+//     // Delete a Node by Value
+//     public static Node deleteNode(Node head, int key) {
+
+//         if (head == null) {
+//             return null;
+//         }
+
+//         // If head itself needs to be deleted
+//         if (head.data == key) {
+//             return head.next;
+//         }
+
+//         Node current = head;
+//         Node prev = null;
+
+//         while (current != null && current.data != key) {
+//             prev = current;
+//             current = current.next;
+//         }
+
+//         // Key not found
+//         if (current == null) {
+//             return head;
+//         }
+
+//         // Delete node
+//         prev.next = current.next;
+
+//         return head;
+//     }
+// }
+
+import java.util.Arrays;
 
 public class Day3 {
 
     public static void main(String[] args) {
 
-        // Creating Linked List: 1 -> 2 -> 3 -> 4
-        Node head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = new Node(4);
+        int[] arr1 = {9, 5, 7, 1};
+        int[] arr2 = {7, 2, 5, 4};
 
-        System.out.println("Original Linked List:");
-        traverse(head);
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
 
-        // Insertion
-        head = insertAtEnd(head, 5);
-        System.out.println("\nAfter Insertion at End:");
-        traverse(head);
+        int[] merged = merge(arr1, arr2);
 
-        // Deletion
-        head = deleteNode(head, 3);
-        System.out.println("\nAfter Deleting 3:");
-        traverse(head);
+        System.out.println("Merged Sorted Array:");
+        System.out.println(Arrays.toString(merged));
     }
 
-    // Traversal
-    public static void traverse(Node head) {
-        Node current = head;
+    public static int[] merge(int[] arr1, int[] arr2) {
 
-        while (current != null) {
-            System.out.print(current.data + " -> ");
-            current = current.next;
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+
+        int[] result = new int[n1 + n2];
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < n1 && j < n2) {
+            if (arr1[i] <= arr2[j]) {
+                result[k] = arr1[i];
+                i++;
+            } else {
+                result[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            result[k] = arr1[i];
+            i++;
+            k++;
         }
 
-        System.out.println("null");
-    }
-
-    // Insert at End
-    public static Node insertAtEnd(Node head, int data) {
-
-        Node newNode = new Node(data);
-
-        if (head == null) {
-            return newNode;
+        while (j < n2) {
+            result[k] = arr2[j];
+            j++;
+            k++;
         }
 
-        Node current = head;
-
-        while (current.next != null) {
-            current = current.next;
-        }
-
-        current.next = newNode;
-
-        return head;
-    }
-
-    // Delete a Node by Value
-    public static Node deleteNode(Node head, int key) {
-
-        if (head == null) {
-            return null;
-        }
-
-        // If head itself needs to be deleted
-        if (head.data == key) {
-            return head.next;
-        }
-
-        Node current = head;
-        Node prev = null;
-
-        while (current != null && current.data != key) {
-            prev = current;
-            current = current.next;
-        }
-
-        // Key not found
-        if (current == null) {
-            return head;
-        }
-
-        // Delete node
-        prev.next = current.next;
-
-        return head;
+        return result;
     }
 }
